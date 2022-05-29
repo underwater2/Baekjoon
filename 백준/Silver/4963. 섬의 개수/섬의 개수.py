c40e659effeedf0507b5ab64337241d161221a64
underwater2 <81406442+underwater2@users.noreply.github.com>
2022-05-29 22:50:25 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 32452 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+# 우하좌상, 대각선 4방향
+di = [0, 1, 0, -1, -1, 1, -1, 1]
+dj = [1, 0, -1, 0, -1, 1, 1, -1]
+
+while True:
+    w, h = map(int, input().split())
+    if not w and not h:
+        break
+    maps = [list(map(int, input().split())) for _ in range(h)]
+
+    islandNum = 0
+    for i in range(h):
+        for j in range(w):
+            if maps[i][j] == 1:
+                # bfs
+                queue = deque([(i, j)])
+                while queue:
+                    x, z = queue.pop()
+                    maps[x][z] = 0
+                    for t in range(8):
+                        nx = x + di[t]
+                        nz = z + dj[t]
+                        if 0 <= nx < h and 0 <= nz < w:
+                            if maps[nx][nz] == 1:
+                                queue.appendleft((nx, nz))
+                                maps[nx][nz] = 0
+                islandNum += 1
+
+    print(islandNum)
+
