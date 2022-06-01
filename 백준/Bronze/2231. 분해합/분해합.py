99789d56fa6021efe6f4573da5c8ff5e810a6856
underwater2 <81406442+underwater2@users.noreply.github.com>
2022-06-01 13:21:40 +0900

    [Bronze II] Title: 분해합, Time: 1260 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,11 @@
+def program(N):
+    for i in range(1, N):
+        result = i
+        for j in str(i):
+            result += int(j)
+        if result == N:
+            return i
+    return 0
+
+N = int(input())
+print(program(N))
