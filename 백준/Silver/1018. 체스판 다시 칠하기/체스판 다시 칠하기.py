70e644d38c89d16cab74cdeecd4a8bdd30f8147d
underwater2 <81406442+underwater2@users.noreply.github.com>
2022-06-01 14:57:45 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,29 +1,27 @@
+# 굳이 새 배열을 잘라 만들지 않고
+# 행, 열 인덱스 합이 짝수인가 홀수인가 따지기
+
 import sys
 
-def count(maps, a, b):
+def count(maps, i, j):
     global minN
     # 0,0 자리가 W일 때
-    cnt = 0
-    for n in range(8):
-        if n in [0, 2, 4, 6]:
-            for m in range(8):
-                if m in [0, 2, 4, 6]:
-                    if maps[n][m] != a:
-                        cnt += 1
-                else:
-                    if maps[n][m] != b:
-                        cnt += 1
-        else:
-            for m in range(8):
-                if m in [0, 2, 4, 6]:
-                    if maps[n][m] != b:
-                        cnt += 1
-                else:
-                    if maps[n][m] != a:
-                        cnt += 1
-    if cnt < minN:
-        minN = cnt
-    # 0,0 자리가 B일 때
+    cntW = 0
+    cntB = 0
+    for n in range(i, i+8):
+        for m in range(j, j+8):
+            if (n+m) % 2:
+                if maps[n][m] != 'B':
+                    cntW += 1
+                if maps[n][m] != 'W':
+                    cntB += 1
+            else:
+                if maps[n][m] != 'W':
+                    cntW += 1
+                if maps[n][m] != 'B':
+                    cntB += 1
+    if min(cntW, cntB) < minN:
+        minN = min(cntW, cntB)
 
 
 N, M = map(int, sys.stdin.readline().split())
@@ -33,10 +31,5 @@ minN = 987654321
 
 for i in range(0, N-7):
     for j in range(0, M-7):
-        newMap = []
-        for k in range(8):
-            newMap.append(maps[i+k][j:j+8])
-        count(newMap, 'W', 'B')
-        count(newMap, 'B', 'W')
+        count(maps, i, j)
 print(minN)
-
