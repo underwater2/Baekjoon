ecb28a8fcec057e73d775b5b7f5d7005a8706bce
underwater2 <81406442+underwater2@users.noreply.github.com>
2022-06-02 15:51:51 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,13 @@
+import sys
+
+from itertools import combinations
+
+dwarf = [int(sys.stdin.readline()) for _ in range(9)]
+
+data = combinations(dwarf, 7)
+
+for i in data:
+    if sum(i) == 100:
+        for height in sorted(list(i)):
+            print(height)
+        break
