3b54cfa1fc98b0bc8783b44a675522fc3134ea02
박윤지 <81406442+underwater2@users.noreply.github.com>
2022-06-13 14:37:43 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 100 ms, Memory: 32416 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,12 @@
+import sys
+
+from collections import deque
+
+N, K = map(int, sys.stdin.readline().split())
+arr = deque([i for i in range(1, N+1)])
+perm = deque()
+
+while arr:
+    arr.rotate(-K+1)
+    perm.append(str(arr.popleft()))
+print('<' + ', '.join(perm) + '>')
