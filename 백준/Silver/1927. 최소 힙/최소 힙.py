baf8e7c7087c13c36fcc98f0aff0315633395813
박윤지 <81406442+underwater2@users.noreply.github.com>
2022-06-15 01:32:26 +0900

    [Silver II] Title: 최소 힙, Time: 372 ms, Memory: 39316 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,17 @@
+import sys
+
+from queue import PriorityQueue
+
+que = PriorityQueue()
+
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x:
+        que.put(x)
+    else:
+        if que.empty():  # if que: 는 제대로 작동 안됨
+            print(0)
+        else:
+            print(que.get())
