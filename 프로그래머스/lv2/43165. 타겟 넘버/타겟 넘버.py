dc4c1286bd7ee45539b94612cde8abc9c5a1b033
박윤지 <81406442+underwater2@users.noreply.github.com>
2022-07-23 11:40:49 +0900

    [level 2] Title: 타겟 넘버, Time: 2.42 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,14 @@
+answer = 0
+
+def dfs(arr, r, total, goal):
+    global answer
+    if r == len(arr):
+        if total == goal:
+            answer += 1
+        return
+    dfs(arr, r+1, total+arr[r], goal)
+    dfs(arr, r+1, total-arr[r], goal)
+
+def solution(numbers, target):
+    dfs(numbers, 0, 0, target)
+    return answer
