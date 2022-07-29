e1e6e5827745b6894e26629a17d850d22de8ac1c
박윤지 <81406442+underwater2@users.noreply.github.com>
2022-07-30 00:53:06 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.66 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,35 @@
+from itertools import combinations
+
+def solution(orders, course):
+    answer = []
+    menus = dict()
+    course.sort(reverse=True)
+    for cnt in course:
+        for order in orders:
+            for comb in combinations(sorted(order), cnt):
+                menu = ''.join(list(comb))
+                if menu in menus:
+                    menus[menu] += 1
+                else:
+                    menus[menu] = 1
+                    
+    sorted_menus = sorted(menus.items(), key = lambda item: item[1], reverse = True)
+    print(sorted_menus)
+    maxN = [0] * 11
+    for menu in sorted_menus:
+        if not course or menu[1] < 2:
+            break
+        elif len(menu[0]) in course:
+            maxN[len(menu[0])] = menu[1]
+            course.remove(len(menu[0]))
+        else:
+            continue
+    print(maxN)
+    
+    for n in range(2, 11):
+        if maxN[n]:
+            for menu in sorted_menus:
+                if len(menu[0]) == n and menu[1] == maxN[n]:
+                    answer.append(menu[0])
+
+    return sorted(set(answer))
