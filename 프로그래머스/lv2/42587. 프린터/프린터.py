d54eb0b0f1d1ad7333816064a8772c4b3481260e
박윤지 <81406442+underwater2@users.noreply.github.com>
2022-08-10 16:16:48 +0900

    [level 2] Title: 프린터, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(priorities, location):
+    queue = deque(priorities)
+    answer = 1
+    while True:
+        maxIdx = queue.index(max(queue))
+        if maxIdx == 0:
+            if location == 0:
+                return answer
+            else:
+                queue.popleft()
+                location -= 1
+                answer += 1
+        else:
+            queue.rotate(-maxIdx)
+            location = (location + len(queue) - maxIdx) % len(queue)
+    
+    return answer
