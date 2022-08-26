1a6494de19442a8928a666439265b1132f943734
박윤지 <81406442+underwater2@users.noreply.github.com>
2022-08-26 23:39:51 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,15 +1,12 @@
-from collections import deque
-
 def solution(number, k):
-    stack = deque()
-    cnt = 0
-    for num in number:
-        while stack and stack[-1] < num and cnt < k:
-            stack.pop()
-            cnt += 1
-        stack.append(num)
-    if cnt != k:
-        return ''.join(stack)[:-(k-cnt)]
-    return ''.join(stack)
-        
-    
+    num = list(number)
+    temp = []
+    for i in range(len(num)-1):
+        temp.append(i)
+        if num[i] < num[i+1]:
+            for t in temp:
+                num[t] = 0
+        elif num[i] == num[i+1]:
+    print(num)
+    answer = ''
+    return answer
