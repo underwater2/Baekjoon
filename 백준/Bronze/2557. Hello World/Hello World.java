36d4924b40d694de2f9ba6f212b18558ce67e1db
박윤지 <81406442+underwater2@users.noreply.github.com>
2024-02-20 12:54:51 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13912 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,6 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+        bw.flush();
     }
 }
