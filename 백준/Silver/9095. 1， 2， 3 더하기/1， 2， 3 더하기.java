e9f2188bd12b30989cf76fc3c25c91dfe0311d85
박윤지 <81406442+underwater2@users.noreply.github.com>
2024-02-27 10:47:20 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            System.out.println(recur(Integer.parseInt(br.readLine())));
+        }
+    }
+
+    public static int recur(int num) {
+        if (num == 1) {
+            return 1;
+        } else if (num == 2) {
+            return 2;
+        } else if (num == 3) {
+            return 4;
+        }
+
+        return recur(num-1) + recur(num-2) + recur(num-3);
+    }
+
+}
